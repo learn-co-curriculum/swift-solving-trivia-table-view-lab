578ec7b9ae02084a9fa8296e54ee355673f292a8
ianrahman <ian.alexander.rahman@gmail.com>
2016-07-21 13:58:26 -0400

    Created Location struct

--- swift-solving-trivia-table-view-lab/StatesTableViewController.swift
+++ swift-solving-trivia-table-view-lab/StatesTableViewController.swift
@@ -11,58 +11,7 @@ import UIKit
 
 class StatesTableViewController: UITableViewController {
     
-    let statesAndCapitals = [
-        "Alabama":"Montgomery",
-        "Alaska":"Juneau",
-        "Arizona":"Phoenix",
-        "Arkansas":"Little Rock",
-        "California":"Sacramento",
-        "Colorado":"Denver",
-        "Connecticut":"Hartford",
-        "Delaware":"Dover",
-        "Florida":"Tallahassee",
-        "Georgia":"Atlanta",
-        "Hawaii":"Honolulu",
-        "Idaho":"Boise",
-        "Illinois":"Springfield",
-        "Indiana":"Indianapolis",
-        "Iowa":"Des Moines",
-        "Kansas":"Topeka",
-        "Kentucky":"Frankfort",
-        "Lousiana":"Baton Rouge",
-        "Maine":"Augusta",
-        "Maryland":"Annapolis",
-        "Massassachusetts":"Boston",
-        "Michigan":"Lansing",
-        "Minnesota":"Saint Paul",
-        "Mississippi":"Jackson",
-        "Missouri":"Jefferson City",
-        "Montana":"Helena",
-        "Nebraska":"Lincoln",
-        "Nevada":"Carson City",
-        "New Hampshire":"Concord",
-        "New Jersey":"Trenton",
-        "New Mexico":"Santa Fe",
-        "New York":"Albany",
-        "North Carolina":"Raleigh",
-        "North Dakota":"Bismarck",
-        "Ohio":"Columbus",
-        "Oklahoma":"Oklahoma City",
-        "Oregon":"Salem",
-        "Pennsylvania":"Harrisburg",
-        "Rhode Island":"Providence",
-        "South Carolina":"Columbia",
-        "South Dakota":"Pierre",
-        "Tennessee":"Nashville",
-        "Texas":"Austin",
-        "Utah":"Salt Lake City",
-        "Vermont":"Montpelier",
-        "Virgina":"Richmond",
-        "Washington":"Olympia",
-        "West Virginia":"Charleston",
-        "Wisconsin":"Madison",
-        "Wyoming":"Cheyenne"
-    ]
+    let statesAndCapitals = Location().statesAndCapitals
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -73,6 +22,7 @@ class StatesTableViewController: UITableViewController {
     }
     
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
         let cell = tableView.dequeueReusableCellWithIdentifier("stateCell", forIndexPath: indexPath)
         
         let sortedStates = statesAndCapitals.keys.sort()
@@ -98,9 +48,9 @@ class StatesTableViewController: UITableViewController {
             let action = UIAlertAction(title: "okay gake", style: .Cancel) { (action) in
                 
             }
-            
+
             alertController.addAction(action)
-            
+
             self.presentViewController(alertController, animated: true) {
                 print("alert presented. neato!")
             }
@@ -110,11 +60,8 @@ class StatesTableViewController: UITableViewController {
     func lettersAreShared(state: String) -> Bool {
         
         if let capital = statesAndCapitals[state]{
-            
             let stateSet = Set(state.lowercaseString.characters).sort()
-            
             let capitalSet = Set(capital.lowercaseString.characters).sort()
-            
             return (stateSet.map { capitalSet.contains($0) }).contains(true)
         }
         
